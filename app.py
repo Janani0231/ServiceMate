c56a3755dbddd014b6ab733bbeee32f304244be5
Janani0231 <jananiks2103@gmail.com>
2024-11-15 06:52:12 +0530

    this goes with the app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask ,render_template, request, redirect, url_for,flash
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object('config.DevelopmentConfig')
 
+app = Flask(__name__)
+
+
+app.config.from_object('config.DevelopmentConfig')
 db = SQLAlchemy(app)
 
 from views import *
